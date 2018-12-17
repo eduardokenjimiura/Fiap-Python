ecdbf20fc26c0c919d447b27efa83d4aaebd47ba
eduardokenjimiura <ekm_1417@msn.com>
2018-12-17 17:21:52 -0200

    fix:(fix nessa merda)

--- nulo.py
+++ nulo.py
@@ -3,6 +3,7 @@ import numpy as np
 df = pd.read_excel('beer_consumption.xlsx', sheetname='Cerveja')
 print(df.convert_objects(convert_numeric=True).dtypes)
  
+ 
 print(df.isnull().any())
 #print(df.columns[df.isnull().any()].tolist())
 dict_gen = dict(zip(range(len(df.columns)),df.columns))
