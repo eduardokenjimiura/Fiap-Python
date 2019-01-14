4c26c4591985f6190ecc2bbe7b85f42e8a540e08
eduardokenjimiura <ekm_1417@msn.com>
2019-01-14 10:46:02 -0200

    Update In[11].py

--- In[11].py
+++ In[11].py
@@ -1,5 +1,5 @@
- 
-#C    rie subsets de treinamento e teste utilizado uma razao adequada de tamanho. Utilze o train_test_split passando como parametros
+
+#namento e teste utilizado uma razao adequada de tamanho. Utilze o train_test_split passando como parametros
  
 from sklearn.model_selection import train_test_split
 import pandas as pd ssssss
